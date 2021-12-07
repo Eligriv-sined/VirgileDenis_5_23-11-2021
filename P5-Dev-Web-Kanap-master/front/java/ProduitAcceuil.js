6ca7c15b3907b939aad7071d59ba8b15fbdfff37
Eligriv-Sined <vd5988@gmail.com>
2021-12-07 14:51:12 +0100

    produit acceuil rajouter

--- P5-Dev-Web-Kanap-master/front/java/ProduitAcceuil.js
+++ P5-Dev-Web-Kanap-master/front/java/ProduitAcceuil.js
@@ -1,6 +1,6 @@
 import {products} from '/P5-Dev-Web-Kanap-master/back/models/Product.js'; //importation tableau product
 
-let produit = document.querySelector('#items');
+
 
 
 document.querySelector('#items').innerHTML = // integration code HTML brute
@@ -8,16 +8,16 @@ document.querySelector('#items').innerHTML = // integration code HTML brute
 
 <a href="./product.html?id=42">
             <article>
-              <img src="./P5-Dev-Web-Kanap-master/back/images/kanap01.jpeg>" alt="${products[0].altTxt}">
-              <h3 class="productName">Kanap name1</h3>
+              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[0].imageUrl}" alt="${products[0].altTxt}">
+              <h3 class="productName">${products[0].name}</h3>
               <p class="productDescription">${products[0].description}</p>
             </article>
           </a>
         
           <a href="./product.html?id=42">
             <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/kanap01.jpeg>" alt="${products[1].altTxt}">
-              <h3 class="productName">Kanap name1</h3>
+              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[1].imageUrl}" alt="${products[1].altTxt}">
+              <h3 class="productName">${products[1].name}</h3>
               <p class="productDescription">${products[1].description}</p>
             </article>
           </a>
@@ -25,8 +25,8 @@ document.querySelector('#items').innerHTML = // integration code HTML brute
          
           <a href="./product.html?id=42">
             <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/kanap01.jpeg>" alt="${products[2].altTxt}">
-              <h3 class="productName">Kanap name1</h3>
+              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[2].imageUrl}" alt="${products[2].altTxt}">
+              <h3 class="productName">${products[2].name}</h3>
               <p class="productDescription">${products[2].description}</p>
             </article>
           </a>
@@ -34,8 +34,8 @@ document.querySelector('#items').innerHTML = // integration code HTML brute
         
           <a href="./product.html?id=42">
             <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/kanap01.jpeg>" alt="${products[3].altTxt}">
-              <h3 class="productName">Kanap name1</h3>
+              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[3].imageUrl}" alt="${products[3].altTxt}">
+              <h3 class="productName">${products[3].name}</h3>
               <p class="productDescription">${products[3].description}</p>
             </article>
           </a>
@@ -43,8 +43,8 @@ document.querySelector('#items').innerHTML = // integration code HTML brute
          
           <a href="./product.html?id=42">
             <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/kanap01.jpeg>" alt="${products[4].altTxt}">
-              <h3 class="productName">Kanap name1</h3>
+              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[4].imageUrl}" alt="${products[4].altTxt}">
+              <h3 class="productName">${products[4].name}</h3>
               <p class="productDescription">${products[4].description}</p>
             </article>
           </a>
@@ -52,8 +52,8 @@ document.querySelector('#items').innerHTML = // integration code HTML brute
          
           <a href="./product.html?id=42">
             <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/kanap01.jpeg>" alt="${products[5].altTxt}">
-              <h3 class="productName">Kanap name1</h3>
+              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[5].imageUrl} " alt="${products[5].altTxt}">
+              <h3 class="productName">${products[5].name}</h3>
               <p class="productDescription">${products[5].description}</p>
             </article>
           </a>
@@ -61,8 +61,8 @@ document.querySelector('#items').innerHTML = // integration code HTML brute
         
           <a href="./product.html?id=42">
             <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/kanap01.jpeg>" alt="${products[6].altTxt}">
-              <h3 class="productName">Kanap name1</h3>
+              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[6].imageUrl}" alt="${products[6].altTxt}">
+              <h3 class="productName">${products[6].name}</h3>
               <p class="productDescription">${products[6].description}</p>
             </article>
           </a>
@@ -70,8 +70,8 @@ document.querySelector('#items').innerHTML = // integration code HTML brute
          
           <a href="./product.html?id=42">
             <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/kanap01.jpeg>" alt="${products[7].altTxt}">
-              <h3 class="productName">Kanap name1</h3>
+              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[7].imageUrl}" alt="${products[7].altTxt}">
+              <h3 class="productName">${products[7].name}</h3>
               <p class="productDescription">${products[7].description}</p>
             </article>
           </a>
