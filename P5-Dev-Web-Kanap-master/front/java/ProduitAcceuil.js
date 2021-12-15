2ca52519fd0c4e2b8c22ea5ff4ff8a229e1716bb
Eligriv-Sined <vd5988@gmail.com>
2021-12-15 15:35:46 +0100

    update product + acceuil

--- P5-Dev-Web-Kanap-master/front/java/ProduitAcceuil.js
+++ P5-Dev-Web-Kanap-master/front/java/ProduitAcceuil.js
@@ -1,83 +1,28 @@
-import {products} from '/P5-Dev-Web-Kanap-master/back/models/Product.js'; //importation tableau product
+let Product =[]; 
 
 
+const fetchProd =  async () => {  //recupération du TABLEAU products au dans la varible Product=[] 
+ await fetch('http://localhost:3000/api/products')
+.then((res) => res.json())
+.then ((data) => (Product = data))
 
+  console.log(Product); //verification du resultat , console
+}
 
-const liensProd = document.querySelector('#items').innerHTML = // integration code HTML brute
+const produtDisplay =  async() => { // Mise en place de l'affichage de product au seins de Items dans index.html
+  await fetchProd();
+  document.getElementById('items').innerHTML = Product.map((products) =>  
+  
+  `<a href="./product.html?id=${products._id}">
+  <article>
+    <img src="${products.imageUrl}" alt="${products.imageUrl}">
+    <h3 class="productName">${products.name}</h3>
+    <p class="productDescription">${products.description}</p>
+  </article>
+</a>`)
+.join("")
+} 
 
-` 
-<a  href="./product.html?id=${products[0]._id}">
-            <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[0].imageUrl}" alt="${products[0].altTxt}">
-              <h3 class="productName">${products[0].name}</h3>
-              <p class="productDescription">${products[0].description}</p>
-            </article>
-          </a>
-        
-          <a " href="./product.html?id=${products[1]._id}">
-            <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[1].imageUrl}" alt="${products[1].altTxt}">
-              <h3 class="productName">${products[1].name}</h3>
-              <p class="productDescription">${products[1].description}</p>
-            </article>
-          </a>
-          </a>
-         
-          <a  href="./product.html?id=${products[2]._id}">
-            <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[2].imageUrl}" alt="${products[2].altTxt}">
-              <h3 class="productName">${products[2].name}</h3>
-              <p class="productDescription">${products[2].description}</p>
-            </article>
-          </a>
-          </a>
-        
-          <a  href="./product.html?id=${products[3]._id}">
-            <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[3].imageUrl}" alt="${products[3].altTxt}">
-              <h3 class="productName">${products[3].name}</h3>
-              <p class="productDescription">${products[3].description}</p>
-            </article>
-          </a>
-          </a>
-         
-          <a href="./product.html?id=${products[4]._id}">
-            <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[4].imageUrl}" alt="${products[4].altTxt}">
-              <h3 class="productName">${products[4].name}</h3>
-              <p class="productDescription">${products[4].description}</p>
-            </article>
-          </a>
-          </a>
-         
-          <a href="./product.html?id=${products[5]._id}">
-            <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[5].imageUrl} " alt="${products[5].altTxt}">
-              <h3 class="productName">${products[5].name}</h3>
-              <p class="productDescription">${products[5].description}</p>
-            </article>
-          </a>
-          </a>
-        
-          <a  href="./product.html?id=${products[6]._id}">
-            <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[6].imageUrl}" alt="${products[6].altTxt}">
-              <h3 class="productName">${products[6].name}</h3>
-              <p class="productDescription">${products[6].description}</p>
-            </article>
-          </a>
-          </a>
-         
-          <a  href="./product.html?id=${products[7]._id}">
-            <article>
-              <img src="/P5-Dev-Web-Kanap-master/back/images/${products[7].imageUrl}" alt="${products[7].altTxt}">
-              <h3 class="productName">${products[7].name}</h3>
-              <p class="productDescription">${products[7].description}</p>
-            </article>
-          </a>
-          ` //fin de lintegration des canap
-
-    
-        
+produtDisplay();
 
 
--- P5-Dev-Web-Kanap-master/front/java/produtcs.js
+++ P5-Dev-Web-Kanap-master/front/java/produtcs.js
@@ -1,82 +1,72 @@
-import {products} from '/P5-Dev-Web-Kanap-master/back/models/Product.js'; //importation tableau product
-
 
 const liensUrl =  window.location.href
-console.log(liensUrl);
+var str = liensUrl ;
+var url = new URL (str);
+var id = url.searchParams.get("id");
+console.log(id); 
+
+
+//--------------------------------------------------------
+let Product =[]; 
+const CouleurProd = document.querySelector("#colors");
+const NombreProd = document.querySelector("#quantity");
+
+//-----------------------------------------------------------
+
+// Récupération des Prod de l'API
+function getProd() {
+  fetch("http://localhost:3000/api/products/" + id)
+  .then((res) => {
+      return res.json();
+  })
+
+  // Répartition des données de l'API dans le DOM
+  .then(async function (resultatAPI) {
+    Product = await resultatAPI;
+      console.table(Product);
+      if (Product){
+        getArticle(Product);
+      }
+  })
+
+}
+
+getProd();
+
+
+function getArticle(Product)  //insertion celon ID produit des element respectif
+
+{
+    
+  // Insertion de l'image
+
+    document.querySelector(".item__img").innerHTML = `<img src="${Product.imageUrl}" alt="${Product.altTxt}"> `
+    
+
+    // Modification du titre "h1"
+
+    document.getElementById('title').innerHTML = `${Product.name} `
+
+    // Modification du prix
+
+    document.getElementById('price').innerHTML = `${Product.price} `
+
+    // Modification de la description
+
+    document.getElementById('description').innerHTML = `${Product.description} `
+
+    // Insertion des options de couleurs
+    
+    for (let couleur of Product.colors){
+        console.table(couleur);
+        let produitCouleur = document.createElement("option");
+        document.querySelector("#colors").appendChild(produitCouleur);
+        produitCouleur.value = couleur;
+        produitCouleur.innerHTML = couleur;
+    }
+    
+}
 
 
 
-//---- Boucle pour produit , chaque if correspond a un produit , importer depuis product
 
-if (liensUrl === 'http://127.0.0.1:5501/P5-Dev-Web-Kanap-master/front/html/product.html?id=107fb5b75607497b96722bda5b504926'){
-    
-const prodimg = document.querySelector('.item__img').innerHTML = `<img src="/P5-Dev-Web-Kanap-master/back/images/${products[0].imageUrl}" alt="${products[0].altTxt}"> `
-const prodNom = document.getElementById('title').innerHTML = `${products[0].name} `
-const prodPrix = document.getElementById('price').innerHTML = `${products[0].price} `
-const prodDescript = document.getElementById('description').innerHTML = `${products[0].description} `
-const prodcouleur = document.getElementById('colors').innerHTML = `<option>${products[0].colors[0]}</option>  <option>${products[0].colors[1]}</option>  <option>${products[0].colors[2]}</option> `
-
-} else if(liensUrl === 'http://127.0.0.1:5501/P5-Dev-Web-Kanap-master/front/html/product.html?id=415b7cacb65d43b2b5c1ff70f3393ad1'){
-    
-    const prodimg = document.querySelector('.item__img').innerHTML = `<img src="/P5-Dev-Web-Kanap-master/back/images/${products[1].imageUrl}" alt="${products[1].altTxt}"> `
-    const prodNom = document.getElementById('title').innerHTML = `${products[1].name} `
-    const prodPrix = document.getElementById('price').innerHTML = `${products[1].price} `
-    const prodDescript = document.getElementById('description').innerHTML = `${products[1].description} `
-    const prodcouleur = document.getElementById('colors').innerHTML = `<option>${products[1].colors[0]}</option>  <option>${products[1].colors[1]}</option> `
-    
-    } else if(liensUrl === 'http://127.0.0.1:5501/P5-Dev-Web-Kanap-master/front/html/product.html?id=055743915a544fde83cfdfc904935ee7'){
-    
-        const prodimg = document.querySelector('.item__img').innerHTML = `<img src="/P5-Dev-Web-Kanap-master/back/images/${products[2].imageUrl}" alt="${products[2].altTxt}"> `
-        const prodNom = document.getElementById('title').innerHTML = `${products[2].name} `
-        const prodPrix = document.getElementById('price').innerHTML = `${products[2].price} `
-        const prodDescript = document.getElementById('description').innerHTML = `${products[2].description} `
-        const prodcouleur = document.getElementById('colors').innerHTML = `<option>${products[2].colors[0]}</option>  <option>${products[2].colors[1]}</option>  <option>${products[2].colors[2]}</option> `
-        
-        } else if(liensUrl === 'http://127.0.0.1:5501/P5-Dev-Web-Kanap-master/front/html/product.html?id=a557292fe5814ea2b15c6ef4bd73ed83'){
-    
-            const prodimg = document.querySelector('.item__img').innerHTML = `<img src="/P5-Dev-Web-Kanap-master/back/images/${products[3].imageUrl}" alt="${products[3].altTxt}"> `
-            const prodNom = document.getElementById('title').innerHTML = `${products[3].name} `
-            const prodPrix = document.getElementById('price').innerHTML = `${products[3].price} `
-            const prodDescript = document.getElementById('description').innerHTML = `${products[3].description} `
-            const prodcouleur = document.getElementById('colors').innerHTML = `<option>${products[3].colors[0]}</option>  <option>${products[3].colors[1]}</option>  `
-            
-            } else if(liensUrl === 'http://127.0.0.1:5501/P5-Dev-Web-Kanap-master/front/html/product.html?id=8906dfda133f4c20a9d0e34f18adcf06'){
-    
-                const prodimg = document.querySelector('.item__img').innerHTML = `<img src="/P5-Dev-Web-Kanap-master/back/images/${products[4].imageUrl}" alt="${products[4].altTxt}"> `
-                const prodNom = document.getElementById('title').innerHTML = `${products[4].name} `
-                const prodPrix = document.getElementById('price').innerHTML = `${products[4].price} `
-                const prodDescript = document.getElementById('description').innerHTML = `${products[4].description} `
-                const prodcouleur = document.getElementById('colors').innerHTML = `<option>${products[4].colors[0]}</option>  <option>${products[4].colors[1]}</option>  <option>${products[4].colors[2]}</option> `
-                
-                } else if(liensUrl === 'http://127.0.0.1:5501/P5-Dev-Web-Kanap-master/front/html/product.html?id=77711f0e466b4ddf953f677d30b0efc9'){
-    
-                    const prodimg = document.querySelector('.item__img').innerHTML = `<img src="/P5-Dev-Web-Kanap-master/back/images/${products[5].imageUrl}" alt="${products[5].altTxt}"> `
-                    const prodNom = document.getElementById('title').innerHTML = `${products[5].name} `
-                    const prodPrix = document.getElementById('price').innerHTML = `${products[5].price} `
-                    const prodDescript = document.getElementById('description').innerHTML = `${products[5].description} `
-                    const prodcouleur = document.getElementById('colors').innerHTML = `<option>${products[5].colors[0]}</option>  <option>${products[5].colors[1]}</option> `
-                    
-                    } else if(liensUrl === 'http://127.0.0.1:5501/P5-Dev-Web-Kanap-master/front/html/product.html?id=034707184e8e4eefb46400b5a3774b5f'){
-    
-                        const prodimg = document.querySelector('.item__img').innerHTML = `<img src="/P5-Dev-Web-Kanap-master/back/images/${products[6].imageUrl}" alt="${products[6].altTxt}"> `
-                        const prodNom = document.getElementById('title').innerHTML = `${products[6].name} `
-                        const prodPrix = document.getElementById('price').innerHTML = `${products[6].price} `
-                        const prodDescript = document.getElementById('description').innerHTML = `${products[6].description} `
-                        const prodcouleur = document.getElementById('colors').innerHTML = `<option>${products[6].colors[0]}</option>  <option>${products[6].colors[1]}</option>  `
-                        
-                        } else if(liensUrl === 'http://127.0.0.1:5501/P5-Dev-Web-Kanap-master/front/html/product.html?id=a6ec5b49bd164d7fbe10f37b6363f9fb'){
-    
-                            const prodimg = document.querySelector('.item__img').innerHTML = `<img src="/P5-Dev-Web-Kanap-master/back/images/${products[7].imageUrl}" alt="${products[7].altTxt}"> `
-                            const prodNom = document.getElementById('title').innerHTML = `${products[7].name} `
-                            const prodPrix = document.getElementById('price').innerHTML = `${products[7].price} `
-                            const prodDescript = document.getElementById('description').innerHTML = `${products[7].description} `
-                            const prodcouleur = document.getElementById('colors').innerHTML = `<option>${products[7].colors[0]}</option>  <option>${products[7].colors[1]}</option>  <option>${products[7].colors[2]}</option> <option>${products[7].colors[3]}</option> `
-                            
-                            } ;
-
-
-                        
-                    
-                
-            
-            
